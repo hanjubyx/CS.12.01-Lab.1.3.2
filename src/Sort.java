1ec8ddfb0162875ca4e3aeee01bb767855ff68a2
hanjubyx <hanjubyx2026@student.cis.edu.hk>
2024-10-08 21:15:00 +0800

    Hi

--- src/Sort.java
+++ src/Sort.java
@@ -1,41 +1,102 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sort {
 
-    public static int[] bubbleSort(int[] array) {
-
-        return array;
-
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
-
-    public static String[] bubbleSort(String[] array) {
-
-        return array;
-
+    public static void bubbleSort(String[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j].compareToIgnoreCase(array[j + 1]) > 0) {
+                    String temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
-
-    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> list) {
-
-        return list;
-
+    public static void bubbleSort(ArrayList<Integer> list) {
+        int n = list.size();
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    Collections.swap(list, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
-
-    public static int[] selectionSort(int[] array) {
-
-        return array;
-
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
+        }
     }
-
-    public static String[] selectionSort(String[] array) {
-
-        return array;
-
+    public static void selectionSort(String[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j].compareToIgnoreCase(array[minIdx]) < 0) {
+                    minIdx = j;
+                }
+            }
+            String temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
+        }
     }
-
-    public static ArrayList<String> selectionSort(ArrayList<String> list) {
-
-        return list;
-
+    public static <T extends Comparable<T>> void selectionSort(ArrayList<T> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                T currentItem = list.get(j);
+                T minItem = list.get(minIdx);
+                if (currentItem instanceof String && minItem instanceof String) {
+                    String currentStr = (String) currentItem;
+                    String minStr = (String) minItem;
+                    if (currentStr.compareToIgnoreCase(minStr) < 0) {
+                        minIdx = j;
+                    }
+                } else {
+                    if (currentItem.compareTo(minItem) < 0) {
+                        minIdx = j;
+                    }
+                }
+            }
+            Collections.swap(list, minIdx, i);
+        }
     }
-
 }
--- src/SortTest.java
+++ src/SortTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SortTest {
 
@@ -15,13 +15,12 @@ class SortTest {
 
     @BeforeEach
     void setUp() {
-        arrayOne = new int[]{12,3,34,12,4,32,11,9,21,25,1,5,19,2,20,13,19,14,1,16};
+        arrayOne = new int[]{12, 3, 34, 12, 4, 32, 11, 9, 21, 25, 1, 5, 19, 2, 20, 13, 19, 14, 1, 16};
         arrayTwo = new String[]{"don't", "tell", "me", "what", "I", "wanna", "hear", "afraid", "of", "never", "knowing", "fear"};
         listOne = new ArrayList<>();
-        for (int i: arrayOne) listOne.add(i);
+        for (int i : arrayOne) listOne.add(i);
         listTwo = new ArrayList<>();
-        for (String str: arrayTwo) listTwo.add(str);
-
+        for (String str : arrayTwo) listTwo.add(str);
     }
 
     @AfterEach
@@ -34,55 +33,43 @@ class SortTest {
 
     @Test
     void bubbleSort() {
-
         int[] sortedArrayOne = {1, 1, 2, 3, 4, 5, 9, 11, 12, 12, 13, 14, 16, 19, 19, 20, 21, 25, 32, 34};
         Sort.bubbleSort(arrayOne);
         for (int i = 0; i < sortedArrayOne.length; i++) assertEquals(sortedArrayOne[i], arrayOne[i]);
-
     }
 
     @Test
     void testBubbleSort() {
-
         String[] sortedArrayTwo = {"afraid", "don't", "fear", "hear", "I", "knowing", "me", "never", "of", "tell", "wanna", "what"};
         Sort.bubbleSort(arrayTwo);
         for (int i = 0; i < sortedArrayTwo.length; i++) assertEquals(sortedArrayTwo[i], arrayTwo[i]);
-
     }
 
     @Test
     void testBubbleSort1() {
-
         int[] sortedArrayOne = {1, 1, 2, 3, 4, 5, 9, 11, 12, 12, 13, 14, 16, 19, 19, 20, 21, 25, 32, 34};
         Sort.bubbleSort(listOne);
         for (int i = 0; i < sortedArrayOne.length; i++) assertEquals(sortedArrayOne[i], listOne.get(i));
-
     }
 
     @Test
     void selectionSort() {
-
         int[] sortedArrayOne = {1, 1, 2, 3, 4, 5, 9, 11, 12, 12, 13, 14, 16, 19, 19, 20, 21, 25, 32, 34};
         Sort.selectionSort(arrayOne);
         for (int i = 0; i < sortedArrayOne.length; i++) assertEquals(sortedArrayOne[i], arrayOne[i]);
-
     }
 
     @Test
     void testSelectionSort() {
-
         String[] sortedArrayTwo = {"afraid", "don't", "fear", "hear", "I", "knowing", "me", "never", "of", "tell", "wanna", "what"};
         Sort.selectionSort(arrayTwo);
         for (int i = 0; i < sortedArrayTwo.length; i++) assertEquals(sortedArrayTwo[i], arrayTwo[i]);
-
     }
 
     @Test
     void testSelectionSort1() {
-
         String[] sortedArrayTwo = {"afraid", "don't", "fear", "hear", "I", "knowing", "me", "never", "of", "tell", "wanna", "what"};
         Sort.selectionSort(listTwo);
         for (int i = 0; i < sortedArrayTwo.length; i++) assertEquals(sortedArrayTwo[i], listTwo.get(i));
-
     }
 }
